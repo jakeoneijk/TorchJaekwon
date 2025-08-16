b8c5689f04ef01a8aa3d775ffee9e94ce9fa217f
jakeoneijk <jakeoneijk@gmail.com>
2025-08-16 10:54:32 +0900

    batch eval

--- torch_jaekwon/evaluate/evaluator/evaluator.py
+++ torch_jaekwon/evaluate/evaluator/evaluator.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import numpy as np
 import torch
 #torchjaekwon import
-from ...util import util_data
+from ...util import util_data, util_torch
 #internal import
 
 class Evaluator():
@@ -15,14 +15,16 @@ class Evaluator():
         pred_dir_path:str,
         gt_dir_path:str,
         evaluation_result_dir:str,
+        batch_size:int = 1, 
         sort_result_by_metric:bool = True,
         device:torch.device = torch.device('cpu')
     ) -> None:
         self.pred_dir_path:str = pred_dir_path
         self.gt_dir_path:str = gt_dir_path
+        self.evaluation_result_dir:str = f'{evaluation_result_dir}/{util_data.get_file_name(self.pred_dir_path)}'
+        self.batch_size:int = batch_size
         self.sort_result_by_metric = sort_result_by_metric
         self.device:torch.device = device
-        self.evaluation_result_dir:str = f'{evaluation_result_dir}/{util_data.get_file_name(self.pred_dir_path)}'
         os.makedirs(self.evaluation_result_dir,exist_ok=True)
     
     '''
@@ -66,8 +68,12 @@ class Evaluator():
         result_dict:dict = self.get_set_wise_result(meta_data_list)
 
         result_dict['result_per_sample'] = list()
+        if self.batch_size > 1:
+            meta_data_list = util_torch.chunk_list(meta_data_list, self.batch_size)
         for meta_data in tqdm(meta_data_list,desc='get result'):
-            result_dict['result_per_sample'].append(self.get_sample_wise_result(meta_data))
+            result = self.get_sample_wise_result(meta_data)
+            if not isinstance(result, list): result = [result]
+            result_dict['result_per_sample'] += result
 
         metric_name_list:list = [metric_name for metric_name in list(result_dict['result_per_sample'][0].keys()) if type(result_dict['result_per_sample'][0][metric_name]) in [float]]
         metric_name_list.sort()
