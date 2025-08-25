0f7c79fbf7759d59b142db28a53e34e1f409c127
jakeoneijk <jakeoneijk@gmail.com>
2025-08-26 02:01:41 +0900

    rsync

---
+++ cp/.script/rsync.sh
@@ -0,0 +1,11 @@
+SERVER="id@domain"
+PORT=22
+TARGET_DIR=""
+SRC_DIR=""
+
+rsync -avz --partial --append --progress -e "ssh -p $PORT" "$SRC_DIR" "$SERVER:$TARGET_DIR"
+#rsync -avz --partial --append --progress -e "ssh -p $PORT" "$SERVER:$SRC_DIR" "$TARGET_DIR"
+
+#CONFIG_FILE="$HOME/.ssh/config"
+#rsync -avz --partial --append --progress -e "ssh -F $CONFIG_FILE" "$SRC_DIR" "$SERVER:$TARGET_DIR"
+#rsync -avz --partial --append --progress -e "ssh -F $CONFIG_FILE" "$SERVER:$SRC_DIR" "$TARGET_DIR"
