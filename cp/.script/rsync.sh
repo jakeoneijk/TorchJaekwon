221d0b9fef5bbff558d65056d6efaf4e7c26804e
jakeoneijk <jakeoneijk@gmail.com>
2025-08-29 15:31:58 +0900

    feature:minor change of rsync

--- cp/.script/rsync.sh
+++ cp/.script/rsync.sh
@@ -3,9 +3,9 @@ PORT=22
 TARGET_DIR=""
 SRC_DIR=""
 
-rsync -avz --partial --append --progress -e "ssh -p $PORT" "$SRC_DIR" "$SERVER:$TARGET_DIR"
-#rsync -avz --partial --append --progress -e "ssh -p $PORT" "$SERVER:$SRC_DIR" "$TARGET_DIR"
+rsync -avz --partial --append-verify --progress -e "ssh -p $PORT" "$SRC_DIR" "$SERVER:$TARGET_DIR"
+#rsync -avz --partial --append-verify --progress -e "ssh -p $PORT" "$SERVER:$SRC_DIR" "$TARGET_DIR"
 
 #CONFIG_FILE="$HOME/.ssh/config"
-#rsync -avz --partial --append --progress -e "ssh -F $CONFIG_FILE" "$SRC_DIR" "$SERVER:$TARGET_DIR"
-#rsync -avz --partial --append --progress -e "ssh -F $CONFIG_FILE" "$SERVER:$SRC_DIR" "$TARGET_DIR"
+#rsync -avz --partial --append-verify --progress -e "ssh -F $CONFIG_FILE" "$SRC_DIR" "$SERVER:$TARGET_DIR"
+#rsync -avz --partial --append-verify --progress -e "ssh -F $CONFIG_FILE" "$SERVER:$SRC_DIR" "$TARGET_DIR"
